81328b55ce16549e2e40b025e858fe87d9e8dc3d
Rick Luna <rluna@neonix.net>
2015-09-23 01:30:27 -0400

    validation added

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-angular.module('app', [])
+angular.module('app', ['ngMessages'])
 .constant('CONSTANTS',{
     version: 0.1,
     vars: {
@@ -11,8 +11,42 @@ angular.module('app', [])
         obnoxious:'',
         hugenumber:'',
         adjective:''
+    },
+    ui: {
+        canShowMadLib: false,
+        canShowForm: true
     }
 })
 .controller('controller1', function(CONSTANTS, $scope) {
     $scope.c = CONSTANTS.vars;
+    $scope.ui = CONSTANTS.ui;
+    
+    $scope.validateForm = function(){
+        console.log($scope.myForm);
+        var canShow = true;
+        var o = $scope.c;
+        for (p in o){
+            if (o.hasOwnProperty(p) && o[p].length === 0){
+                canShow = false;
+                console.log(p,o[p].length);
+                break;
+            }
+        }
+        console.log(canShow);
+        if (canShow){
+            $scope.ui.canShowForm = false;
+            $scope.ui.canShowMadLib = true;   
+        }
+    };
+    
+    $scope.startOver = function(){
+        var o = $scope.c;
+        for (p in o){
+            o.hasOwnProperty(p) && (o[p] = '');
+        }
+        $scope.ui.canShowForm = true;
+        $scope.ui.canShowMadLib = false;
+        $scope.myForm.$setPristine();
+    };
+    
 });
